890174e226e6be264725eb93c92b4ffd9c6980ff
최지연 <choi02647@naver.com>
2023-08-01 19:08:27 +0900

    ui done

---
+++ AppleFramework/FrameworkCell.swift
@@ -0,0 +1,25 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework
+//
+//  Created by 최지연/클라이언트 on 2023/08/01.
+//
+
+import UIKit
+
+class FrameworkCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameLabel.numberOfLines = 1
+        nameLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func configure(_ framework: AppleFramework) {
+        thumbnailImageView.image = UIImage(named: framework.imageName)
+        nameLabel.text = framework.name
+    }
+}
